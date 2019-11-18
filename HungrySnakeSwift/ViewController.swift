2c4eb9c4e31550abc91c7d14c5cf39fe9141c270
Gordon <hellogordon.f@gmail.com>
2019-11-18 11:07:21 +0800

    Initial Commit

---
+++ HungrySnakeSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HungrySnakeSwift
+//
+//  Created by Kuan-Chieh Feng on 2019/11/18.
+//  Copyright © 2019 Kuan-Chieh Feng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
